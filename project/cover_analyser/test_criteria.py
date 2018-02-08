feb2db75641c529d629caafa60ac815af11e9f7a
rmulton <rmulton@gmail.com>
2018-02-08 16:46:55 +0100

    implémentation de test() pour KTC

--- project/cover_analyser/test_criteria.py
+++ project/cover_analyser/test_criteria.py
@@ -19,7 +19,7 @@ class TD:
         if_while_edges = set(program.get_if_while_edges())
         # Run the program for all the test set, and remove labels of visited nodes
         for test in test_set:
-            # Get te path for 
+            # Get the path for the test
             path = program.get_path(test)
             for node_or_edge in path:
                 # If it is a node
@@ -39,7 +39,20 @@ class K_TC:
         self.k = k
         return
     def test(self, test_set, program):
-        return
+        # Get the paths of length inferior or equal to k
+        k_paths = program.get_k_paths(self.k)
+        # Remove from k_paths the paths that the program follows when using the test set
+        for test in test_set:
+            # Get the path for the test
+            path = program.get_path(test)
+            # If the path is in the paths of length inferior or equal to k, remove it
+            if path in k_paths:
+                k_paths.remove(path)
+        # If k_paths is empty, the test pass, otherwise it doesn't
+        if len(k_paths)==0:
+            return True
+        else:
+            return False
 
 class I_TB:
     def __init__(self, i):
