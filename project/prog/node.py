71f4a5654782c811470e977ab942d33bcb858d23
rmulton <rmulton@gmail.com>
2018-02-08 13:18:53 +0100

    added end node

---
+++ project/prog/node.py
@@ -0,0 +1,15 @@
+class Always:
+    def __init__(self):
+        return
+
+class If:
+    def __init__(self):
+        return
+
+class While:
+    def __init__(self):
+        return
+
+class End:
+    def __init__(self):
+        return
