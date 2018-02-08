ce720dbdbd3fdabaafe7801d4fb527bf95a104d6
rmulton <rmulton@gmail.com>
2018-02-08 15:39:21 +0100

    program structure

---
+++ project/cover_analyser/test_criteria.py
@@ -0,0 +1,50 @@
+class TA:
+    def __init__(self):
+        return
+    def test(self, test_set, program_graph):
+        return
+
+class TD:
+    def __init__(self):
+        return
+    def test(self, test_set, program_graph):
+        return
+
+class K_TC:
+    def __init__(self, k):
+        self.k = k
+        return
+    def test(self, test_set, program_graph):
+        return
+
+class I_TB:
+    def __init__(self, i):
+        self.i = i
+        return
+    def test(self, test_set, program_graph):
+        return
+
+class TDef:
+    def __init__(self):
+        return
+    def test(self, test_set, program_graph):
+        return
+
+class TU:
+    def __init__(self):
+        return
+    def test(self, test_set, program_graph):
+        return
+
+class TDU:
+    def __init__(self):
+        return
+    def test(self, test_set, program_graph):
+        return
+
+class TC:
+    def __init__(self):
+        return
+    def test(self, test_set, program_graph):
+        return
+
---
+++ project/prog/Program.py
@@ -0,0 +1,21 @@
+class Program:
+    def __init__(self, program_graph):
+        self.program_graph = program_graph
+    
+    # Used for TA criterium
+    def get_assign_labels(self):
+        return
+    
+    # Used for TD
+    def get_if_while_edges(self):
+        return
+    
+    # Used for K_TC
+    def get_k_paths(self, k):
+        return
+    
+    # Used for I_TB
+    def get_i_while_loops(self, i):
+        return
+    
+    # TODO: TU, TDU, TC
