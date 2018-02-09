c885845ef74e445c4788f867a5345443545cae85
rmulton <rmulton@gmail.com>
2018-02-09 19:23:43 +0100

    cleaned tests

--- project/test_prog.py
+++ project/test_prog.py
@@ -7,6 +7,7 @@ if __name__=="__main__":
     pgm_while = examples.create_test_program2()
     pgm_while_2 = examples.create_test_program3()
 
+    print("=== First program ===")
     # Test the TA criterion
     ta_test_set = [{"x": 0},{"x": -1},{"x": 1}]
     tests.test_ta(ta_test_set, pgm)
@@ -19,14 +20,30 @@ if __name__=="__main__":
     ktc_test_set = [{"x": 0}, {"x": 5}]
     tests.test_ktc(ktc_test_set, pgm, 2)
 
-    # Test the ITB criterium
-    itb_tests_set = [{"x": -2},{"x": -1},{"x": 5}]
-    tests.test_itb(itb_tests_set,pgm_while,1)
-    tests.test_itb(itb_tests_set, pgm_while_2 ,1)
-
     # Test the TDef criterium
     test_set = [{"x": 0}, {"x": 5}, {"x": -1}]
     tests.test_tdef(test_set, pgm)
+
+    # Test the TC criterium
+    tc_test_set = [{"x": 0}, {"x": 5}, {"x": -1}]
+    tests.test_tc(tc_test_set, pgm)
+
+    # Test the ITB criterium
+    itb_tests_set = [{"x": -2},{"x": -1},{"x": 5}]
+    tests.test_itb(itb_tests_set, pgm ,1)
+
+    # Test the TU criterium
+    tu_test_set = [{"x": -2},{"x": -1},{"x": 5}]
+    tests.test_tu(tu_test_set, pgm)
+
+    # Test the TDU criterium
+    tdu_test_set = [{"x": -2},{"x": -1},{"x": 5}]
+    tests.test_tdu(tu_test_set, pgm)
+
+    print("=== End for first program ===")
+    # Test the ITB criterium
+    itb_tests_set = [{"x": -2},{"x": -1},{"x": 5}]
+    tests.test_itb(itb_tests_set,pgm_while,1)
 
     # Test the TU criterium
     tu_test_set = [{"x": -2},{"x": -1},{"x": 5}]
@@ -36,6 +53,4 @@ if __name__=="__main__":
     tdu_test_set = [{"x": -2},{"x": -1},{"x": 5}]
     tests.test_tdu(tu_test_set,pgm_while)
 
-    # Test the TC criterium
-    tc_test_set = [{"x": 0}, {"x": 5}, {"x": -1}]
-    tests.test_tc(tc_test_set, pgm)
+
